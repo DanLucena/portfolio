a09105d87212d24a8979380cd593631a57c07d95
DanLucena <danlucas1711@gmail.com>
2023-01-05 02:33:09 -0300

    [+] Adding urls to project cards

--- src/lib/helpers/projects.ts
+++ src/lib/helpers/projects.ts
@@ -2,21 +2,25 @@ export const projects = [
   {
     title: 'Brasil Sem Fake',
     description: 'This was a project developed in college, where we made a cell phone application that, through natural language interpretation, tries to identify whether a piece of news is false or not.',
-    projectStack: ['Flutter', 'Python']
+    projectStack: ['Flutter', 'Python'],
+    link: 'https://github.com/DanLucena/BrasilSemFake'
   },
   {
     title: 'Traveller Frontend',
     description: 'An application made with the intention of using graphs in practice, creating a system that finds the best path of a route made by the user.',
-    projectStack: ['NextJS', 'Typescript']
+    projectStack: ['NextJS', 'Typescript'],
+    link: 'https://github.com/DanLucena/PI-Grafos-Frontend'
   },
   {
     title: 'Traveller Backend',
     description: 'An application made with the intention of using graphs in practice, creating a system that finds the best path of a route made by the user.',
-    projectStack: ['NodeJS', 'Typescript']
+    projectStack: ['NodeJS', 'Typescript'],
+    link: 'https://github.com/DanLucena/PI-Grafos'
   },
   {
     title: 'Authentication Micro Service',
     description: 'A login microservice that aims to be simple, reusable and detachable, thus being able to be used in several applications with different purposes.',
-    projectStack: ['NestJS', 'NodeJS', 'Typescript']
+    projectStack: ['NestJS', 'NodeJS', 'Typescript'],
+    link: '#'
   },
 ]
